4c668a88986f87a3e3a6f4f63b102c5ce1b97cd5
michael <connellmp@gmail.com>
2020-11-12 13:17:30 -0800

    some parser testing

--- fql.py
+++ fql.py
@@ -1,8 +1,12 @@
 import parser
 
-TEST_TEXT = """EXCLUDING (COLUMN2) SELECT
+test_text = """EXCLUDING (COLUMN2) SELECT
  *, 10 * 10,
 FROM TEST_TABLE
-WHERE 1 = 1"""
+WHERE 1.24 = 1.67"""
 
-lex = parser.Lexer("fql.py", TEST_TEXT)
+test_text = " ".join(test_text.lower().split()).replace(',', '')
+
+print(test_text)
+
+lex = parser.Lexer("fql.py", test_text)
