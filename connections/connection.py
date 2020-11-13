e0f3ad684a8086fd7b74f7f56f421bb66b620e0c
michael <connellmp@gmail.com>
2020-11-12 22:42:23 -0800

    define setconnection class

--- connections/connection.py
+++ connections/connection.py
@@ -1,10 +1,18 @@
 from dataclasses import dataclass
+from typing import Optional
+
 import pandas as pd
 
 
 @dataclass
 class SetConnection:
-    db: str
+    driver: str
+    host: str
+    database: str
+    user: str
+    password: str
+    port: Optional[int] = 8000
+
 
 df = pd.DataFrame()
 
@@ -13,8 +21,11 @@ df = pd.DataFrame()
 #      Postgres
 #      SQLite
 
+driver = "sql server"
+host = "192.168.1.1"
+database = "PROD"
+user = "SA"
+password = "hunter2"
+port = 8000
 
-"""
-    from connections import connection
-    connection.SetConnection(db = "mssql")
-"""
+s = SetConnection(driver, host, database, user, password, port)
