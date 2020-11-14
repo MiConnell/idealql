c7521fbf31aba5d425e083be5a6ec64a757016d0
michael <connellmp@gmail.com>
2020-11-13 16:19:21 -0800

    add delete preview functionality

---
+++ utils/previewer.py
@@ -0,0 +1,19 @@
+import parser
+import sys
+sys.path.append('../')
+
+def execute(query):
+    return query
+
+
+# class Preview(parser.Lexer):
+class Preview:
+
+    def __init__(self) -> None:
+        self.query = "PREVIEW DELETE FROM TABLE WHERE 51 % 17 = 0"
+
+    def _replace_delete(self):
+        return self.query.upper().replace("PREVIEW ", "").replace("DELETE", "SELECT")
+
+    def to_be_deleted(self):
+        return execute(self._replace_delete())
