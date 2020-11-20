fdb326dd515d8143f7e2043bb0c71a8938b8b72f
michael <connellmp@gmail.com>
2020-11-20 10:10:33 -0800

    add credential file parsing logic

--- fql.py
+++ fql.py
@@ -1,36 +1,47 @@
-from connections.connection import SetConnection
-
 from utils import converter, parser, previewer
 from connections import connection
 
-conn = [
-    'mssql',
-    'localhost',
-    'dbname',
-    'sa',
-    'someThingComplicated1234'
-]
+import argparse
+
+ps = argparse.ArgumentParser(description="Run FQL File.")
+ps.add_argument(
+    "file name", metavar="Filename", type=str, nargs=1, help=".fql file to run"
+)
+ps.add_argument("-c", "--credentials", help="set the connection file destination")
+args = ps.parse_args()
+
+if args.credentials:
+    conn = connection.get_connection(args.credentials)
+else:
+    conn = connection.get_connection()
+
+class InvalidConnectionError(Exception):
+    pass
 
 
 class FQL:
-    def __init__(self):
+    def __init__(self, ag):
+        self.ag = ag
         return None
 
     def __repr__(self) -> str:
-        return f'FQL Connection: {self.conn}'
+        return f"FQL Connection: {self.conn}"
 
-    def initialize(self, conn):
-        self.conn = connection.SetConnection(*conn)
+    def initialize(self):
+        try:
+            self.conn = connection.SetConnection(*conn)
+        except FileNotFoundError:
+            raise FileNotFoundError(
+                f"""credentials.json file not found in default location '{connection.__location__}'!
+
+Either add the file there or set a new file name and location with --credentials (or -c)."""
+            )
 
     def main(self):
         return self
 
+
 if __name__ == "__main__":
-    reader = FQL()
+    reader = FQL(args)
+    reader.initialize()
     reader.main()
-
-
-f = FQL()
-f.initialize(conn)
-
-print(f)
