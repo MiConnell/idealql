c4224bbe959914a7c5d3ec695443b3f16ada1bdc
michael <connellmp@gmail.com>
2020-11-20 13:55:48 -0800

    add star to select

--- utils/previewer.py
+++ utils/previewer.py
@@ -9,7 +9,7 @@ class Preview(parser.Lexer):
         self.query = query
 
     def _replace_delete(self):
-        return self.query.upper().replace("PREVIEW ", "").replace("DELETE", "SELECT")
+        return self.query.upper().replace("PREVIEW ", "").replace("DELETE", "SELECT *")
 
     def to_be_deleted(self):
         return execute(self._replace_delete())
